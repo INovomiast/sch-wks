87d36a04e26406ce443ab00bfbb22a27695e46e5
inovomiast <inovomiast@hotmail.com>
2022-04-29 22:29:09 +0200

    AXK

--- src/js/wrk-visualization.js
+++ src/js/wrk-visualization.js
@@ -19,8 +19,14 @@ const work_ids = ['dPzsFYA'];
 
 if(work_id === work_ids[0]) {
     document.title = 'Tipos de Señales - WorkVisualizer - School Works';
-    document.getElementById('content').innerHTML = '<object type="text/html" data="../../../src/extra/works/tds.tecno.html"></object>';
+    document.getElementById('content').innerHTML = '<object type="text/html" data="../../../src/extra/works/tds.tecno.html" id="page-content"></object>';
+    const pg_content = document.getElementById('page-content');
+    pg_content.style.width = '100%';
+    pg_content.style.height = '100%';
 } else {
     document.title = 'Trabajo Inexistente - WorkVisualizer - Shchool Works';
-    document.getElementById('content').innerHTML = '0'
+    document.getElementById('content').innerHTML = '<object type="text/html" data="../../../src/extra/error.html" id="page-content" width="1" height="1500px"></object>';
+    const pg_content = document.getElementById('page-content');
+    pg_content.style.width = '100%';
+    pg_content.style.height = '100%';
 }
